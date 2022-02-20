9f8c26eba62d2a971025c2e56800cb6c3677322b
Makurin Maksim <antidod89@gmail.com>
2022-02-20 22:44:25 +0300

    lesson 1/ home work

---
+++ Lesson_1/Home_Work/index.js
@@ -0,0 +1,20 @@
+const nameEarth = "Земля";
+console.log(nameEarth);
+
+let userLocalTime = "21:05:54";
+console.log(userLocalTime);
+
+let numberArticles = 13;
+console.log(numberArticles);
+
+let isCash = false;
+console.log(isCash);
+
+let lastName = "Макурин";
+console.log(lastName);
+
+let firstName = "Максим";
+console.log(firstName);
+
+let patronym = "Валерьевич";
+console.log(patronym);
