b0e9373f61901c9f484c7a6ae4935431bf148a6c
Maksim Makurin <antidod89@gmail.com>
2022-03-04 15:44:57 +0300

    Home work js lesson 1

--- Lesson_1/Home_Work/index.js
+++ Lesson_1/Home_Work/index.js
@@ -1,20 +1,102 @@
-const nameEarth = "Земля";
-console.log(nameEarth);
+// const nameEarth = "Земля";
+// console.log(nameEarth);
 
-let userLocalTime = "21:05:54";
-console.log(userLocalTime);
+// let userLocalTime = "21:05:54";
+// console.log(userLocalTime);
 
-let numberArticles = 13;
-console.log(numberArticles);
+// let numberArticles = 13;
+// console.log(numberArticles);
 
-let isCash = false;
-console.log(isCash);
+// let isCash = false;
+// console.log(isCash);
 
-let lastName = "Макурин";
-console.log(lastName);
+// let lastName = "Макурин";
+// console.log(lastName);
 
-let firstName = "Максим";
-console.log(firstName);
+// let firstName = "Максим";
+// console.log(firstName);
 
-let patronym = "Валерьевич";
-console.log(patronym);
+// let patronym = "Валерьевич";
+// console.log(patronym);
+
+
+// 04.03.22
+
+// console.log("Hello World!")
+
+
+// Number
+// const ageNumber = 33;
+// console.log(ageNumber);
+// console.log(typeof ageNumber);
+// console.log(typeof (ageNumber));
+
+//String
+// const userName = "Maksim";
+// console.log(userName);
+// console.log(typeof userName);
+// console.log(typeof (userName));
+
+// userName = "Ivan";
+// console.log(userName);
+
+//Boolean
+// let isAdmin = true;
+// console.log(isAdmin);
+// console.log(typeof isAdmin);
+// console.log(typeof (isAdmin));
+
+// null
+// let nameNull = null;
+// console.log(nameNull);
+// console.log(typeof nameNull);
+// console.log(typeof (nameNull));
+
+//undefined
+// let lastName
+// console.log(lastName);
+// console.log(typeof lastName);
+// console.log(typeof (lastName));
+
+//object
+// const user1 = {
+//     name: "Petya",
+//     age: 18,
+//     city: "N.Novgorod"
+// };
+// console.log(user.city);
+// console.log(typeof user.age);
+// console.log(typeof (user.name));
+// console.log(typeof user);
+// user = 12;
+// user.name = "Maksim"
+// console.log(user.name);
+// let user2 = {
+//     name: "Petya",
+//     age: 18,
+//     city: "N.Novgorod"
+// };
+// console.log(user2);
+// user2 = "Vasya";
+// console.log(user2);
+var user3 = {
+    name: "Petya",
+    age: 18,
+    city: "N.Novgorod"
+};
+console.log(user3);
+user3 = "Vasya";
+user3.age = 20;
+console.log(user3.age);
+
+
+//Symbol
+// let sym = Symbol("foo");
+// console.log(sym);
+// console.log(typeof sym);
+// console.log(typeof (sym));
+//Bigint
+// const sameBigint = BigInt("1234567890123456789012345678901234567890");
+// console.log(sameBigint);
+// console.log(typeof sameBigint);
+// console.log(typeof (sameBigint));
