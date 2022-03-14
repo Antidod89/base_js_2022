c7d26f828952a4f0a1dbb45c3cf9ec60bbcb7aea
Maksim Makurin <antidod89@gmail.com>
2022-03-14 15:58:12 +0300

    home work 5

---
+++ Lesson_5/Home_work/index.js
@@ -0,0 +1,63 @@
+// 1. Сделайте функцию, которая возвращает квадрат числа. Число передается параметром.
+
+// let getSquareOfNumber = (calcValue) => {
+//     const result = calcValue ** 2;
+//     return result;
+// };
+
+// console.log(getSquareOfNumber(5));
+
+// 2. Сделайте функцию, которая возвращает сумму двух чисел.
+
+// function calcValue(a, b) {
+//     return a + b;
+// }
+// const result = calcValue(5, 10)
+// console.log(result);
+
+// 3. Сделайте функцию, которая отнимает от первого числа второе и делит на третье.
+
+// function calcValue(a, b, c) {
+//         return (a - b) / c ;
+//     }
+
+// const result = calcValue(30, 10, 5)
+// console.log(result);
+
+// 4. Сделайте функцию, которая принимает параметром число от 1 до 7, а возвращает день недели на русском языке.
+
+
+// const value = 1;
+
+// const getDayOfTheWeek = (day) => {
+//     switch (day) {
+//         case 1:
+//           console.log("Понедельник");
+//           break;
+//         case 2:
+//           console.log("Вторник");
+//           break;
+//         case 3:
+//           console.log("Среда");
+//           break;
+//         case 4:
+//           console.log("Четверг");
+//           break;
+//         case 5:
+//           console.log("Пятница");
+//           break;
+//         case 6:
+//           console.log("Суббота");
+//           break;  
+//         case 7:
+//           console.log("Воскресенье");
+//           break;
+//         default:
+//             console.log("Число не соответствует дню недели");
+// }
+// }
+
+// getDayOfTheWeek(value);
+
+// 5. Сделайте функцию, которая параметрами принимает 2 числа. Если эти числа равны - пусть функция вернет true, а если не равны - false.
+
